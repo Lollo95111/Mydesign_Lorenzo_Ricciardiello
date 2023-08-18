d5ed10a378914b2afea54853338305dfa1a70376
Lorenzo Ricciardiello <lorenzo1995111@gmail.com>
2023-08-18 18:24:43 +0100

    portatile

---
+++ public/welcome.js
@@ -0,0 +1,18 @@
+// elements
+var elements_to_watch = document.querySelectorAll('.watch');
+// callback 
+var callback = function(items){
+  items.forEach((item) => {
+    if(item.isIntersecting){
+      item.target.classList.add("in-page");
+    } else{
+      item.target.classList.remove("in-page");
+    }
+  });
+}
+// observer
+var observer = new IntersectionObserver(callback, { threshold: 0.6 } );
+// apply
+elements_to_watch.forEach((element) => {
+  observer.observe(element); 
+});
